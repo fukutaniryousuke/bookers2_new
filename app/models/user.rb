c0aa2167388555ba99de8060fe57323a7f733e51
EC2 Default User <[Fr83653554@gmail.com]>
2022-05-03 02:50:51 +0000

    [Add]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
